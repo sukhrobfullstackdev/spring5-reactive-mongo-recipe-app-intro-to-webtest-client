cb03747122c02b122884b0e0f1b34b22885d1983
sukhrobdev <suxrobdevbk@gmail.com>
2022-11-11 16:47:38 +0500

    Reactive programming and its validation

---
+++ src/test/java/guru/springframework/Spring5RecipeAppApplicationTests.java
@@ -0,0 +1,15 @@
+package guru.springframework;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+public class Spring5RecipeAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
